c1c1a77df743ecada855a719950cf54bd3370fa4
llchuj <1690858147@qq.com>
2025-03-20 15:40:16 +0800

    完成练习 9 作业，添加 CustomPizza 类等功能

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-public class Main{
-    public static void main(String[] args) throws Exception {
+public class Main {
+    public static void main(String[] args) {
         HandleOrders handleOrders = new HandleOrders();
         handleOrders.takeOrder();
         handleOrders.createOrderSummary();
         System.out.println(handleOrders);
+        handleOrders.displayCustomPizzas();
+
+        OrderLogs orderLogs = new OrderLogs();
+        orderLogs.addOrderLog("Order 1 placed");
+        orderLogs.addOrderLog("Order 2 placed");
+        orderLogs.handleLogs();
+
+        OrderQueue orderQueue = new OrderQueue();
+        orderQueue.handleQueue();
     }
 }
